a7038cbc0e538f1824723926d08ae6e7a02602ad
tnaskali <thomas@naska.li>
2023-08-23 13:39:53 +0300

    set security configuration to permitAll

--- src/main/java/li/naska/bgg/configuration/SecurityConfiguration.java
+++ src/main/java/li/naska/bgg/configuration/SecurityConfiguration.java
@@ -4,10 +4,12 @@ import li.naska.bgg.security.BggAuthenticationManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.savedrequest.NoOpServerRequestCache;
 
 @EnableWebFluxSecurity
 @Configuration
@@ -20,14 +22,13 @@ public class SecurityConfiguration {
   public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
     return http
         .authenticationManager(authenticationManager)
+        .requestCache(spec -> spec
+            .requestCache(NoOpServerRequestCache.getInstance()))
         .csrf(CsrfSpec::disable)
         .authorizeExchange(spec -> spec
-            .pathMatchers("/api/*/*/current/**")
-            .authenticated()
             .anyExchange()
             .permitAll())
-        .httpBasic(spec -> {
-        })
+        .httpBasic(Customizer.withDefaults())
         .build();
   }
 
