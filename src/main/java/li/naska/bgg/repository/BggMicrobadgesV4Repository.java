1a30aad2c1a6233d5d87babcb737346103b72256
Thomas Naskali <thomas@naska.li>
2024-10-14 20:55:55 +0300

    rework microbadges v4

--- src/main/java/li/naska/bgg/repository/BggMicrobadgesV4Repository.java
+++ src/main/java/li/naska/bgg/repository/BggMicrobadgesV4Repository.java
@@ -1,30 +1,29 @@
 package li.naska.bgg.repository;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
+import li.naska.bgg.exception.UnexpectedServerResponseException;
 import li.naska.bgg.repository.model.BggMicrobadgesV4ResponseBody;
-import org.springframework.beans.factory.annotation.Autowired;
+import li.naska.bgg.util.JsonProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 @Repository
 public class BggMicrobadgesV4Repository {
 
-  @Autowired
-  private ObjectMapper objectMapper;
-
   private final WebClient webClient;
 
+  private final JsonProcessor jsonProcessor;
+
   public BggMicrobadgesV4Repository(
-      @Autowired WebClient.Builder builder,
-      @Value("${bgg.endpoints.v4.microbadges}") String endpoint) {
+      @Value("${bgg.endpoints.v4.microbadges}") String endpoint,
+      WebClient.Builder builder,
+      JsonProcessor jsonProcessor) {
     this.webClient = builder.baseUrl(endpoint).build();
+    this.jsonProcessor = jsonProcessor;
   }
 
   public Mono<BggMicrobadgesV4ResponseBody> getMicrobadge(Integer id) {
@@ -33,15 +32,14 @@ public class BggMicrobadgesV4Repository {
         .uri(uriBuilder -> uriBuilder.path("/{id}").build(id))
         .accept(MediaType.APPLICATION_XML)
         .acceptCharset(StandardCharsets.UTF_8)
-        .retrieve()
-        .toEntity(String.class)
-        .<BggMicrobadgesV4ResponseBody>handle((entity, sink) -> {
-          try {
-            sink.next(objectMapper.readValue(entity.getBody(), BggMicrobadgesV4ResponseBody.class));
-          } catch (JsonProcessingException e) {
-            sink.error(
-                new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+        .exchangeToMono(clientResponse -> {
+          if (clientResponse.statusCode() != HttpStatus.OK) {
+            return UnexpectedServerResponseException.from(clientResponse).buildAndThrow();
           }
+          return clientResponse
+              .bodyToMono(String.class)
+              .defaultIfEmpty("")
+              .map(body -> jsonProcessor.toJavaObject(body, BggMicrobadgesV4ResponseBody.class));
         });
   }
 }
