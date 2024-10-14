861903c458646a7350967232cb105bda627ad5d4
Thomas Naskali <thomas@naska.li>
2024-10-14 21:08:01 +0300

    rework threads v4

--- src/main/java/li/naska/bgg/repository/BggThreadsV4Repository.java
+++ src/main/java/li/naska/bgg/repository/BggThreadsV4Repository.java
@@ -1,32 +1,32 @@
 package li.naska.bgg.repository;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
+import li.naska.bgg.exception.UnexpectedServerResponseException;
 import li.naska.bgg.repository.model.BggThreadReactionsV4QueryParams;
 import li.naska.bgg.repository.model.BggThreadReactionsV4ResponseBody;
 import li.naska.bgg.repository.model.BggThreadV4ResponseBody;
+import li.naska.bgg.util.JsonProcessor;
 import li.naska.bgg.util.QueryParameters;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 @Repository
 public class BggThreadsV4Repository {
 
-  @Autowired
-  private ObjectMapper objectMapper;
-
   private final WebClient webClient;
 
+  private final JsonProcessor jsonProcessor;
+
   public BggThreadsV4Repository(
-      @Autowired WebClient.Builder builder, @Value("${bgg.endpoints.v4.threads}") String endpoint) {
+      @Value("${bgg.endpoints.v4.threads}") String endpoint,
+      WebClient.Builder builder,
+      JsonProcessor jsonProcessor) {
     this.webClient = builder.baseUrl(endpoint).build();
+    this.jsonProcessor = jsonProcessor;
   }
 
   public Mono<BggThreadV4ResponseBody> getThread(Integer id) {
@@ -35,15 +35,14 @@ public class BggThreadsV4Repository {
         .uri(uriBuilder -> uriBuilder.path("/{id}").build(id))
         .accept(MediaType.APPLICATION_XML)
         .acceptCharset(StandardCharsets.UTF_8)
-        .retrieve()
-        .toEntity(String.class)
-        .<BggThreadV4ResponseBody>handle((entity, sink) -> {
-          try {
-            sink.next(objectMapper.readValue(entity.getBody(), BggThreadV4ResponseBody.class));
-          } catch (JsonProcessingException e) {
-            sink.error(
-                new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+        .exchangeToMono(clientResponse -> {
+          if (clientResponse.statusCode() != HttpStatus.OK) {
+            return UnexpectedServerResponseException.from(clientResponse).buildAndThrow();
           }
+          return clientResponse
+              .bodyToMono(String.class)
+              .defaultIfEmpty("")
+              .map(body -> jsonProcessor.toJavaObject(body, BggThreadV4ResponseBody.class));
         });
   }
 
@@ -57,20 +56,15 @@ public class BggThreadsV4Repository {
             .build(id))
         .accept(MediaType.APPLICATION_XML)
         .acceptCharset(StandardCharsets.UTF_8)
-        .retrieve()
-        .onStatus(
-            httpStatus -> httpStatus == HttpStatus.BAD_REQUEST,
-            clientResponse -> Mono.error(
-                new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unknown remote error")))
-        .toEntity(String.class)
-        .<BggThreadReactionsV4ResponseBody>handle((entity, sink) -> {
-          try {
-            sink.next(
-                objectMapper.readValue(entity.getBody(), BggThreadReactionsV4ResponseBody.class));
-          } catch (JsonProcessingException e) {
-            sink.error(
-                new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+        .exchangeToMono(clientResponse -> {
+          if (clientResponse.statusCode() != HttpStatus.OK) {
+            return UnexpectedServerResponseException.from(clientResponse).buildAndThrow();
           }
+          return clientResponse
+              .bodyToMono(String.class)
+              .defaultIfEmpty("")
+              .map(
+                  body -> jsonProcessor.toJavaObject(body, BggThreadReactionsV4ResponseBody.class));
         });
   }
 }
