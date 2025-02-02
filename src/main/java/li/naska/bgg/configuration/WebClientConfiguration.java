b3acd32a77a7e1d13e744220ee1a1c561388d162
Thomas Naskali <thomas@naska.li>
2025-02-02 13:58:08 +0200

    use WebClientCustomizer instead to redefining the WebClient.Builder

--- src/main/java/li/naska/bgg/configuration/WebClientConfiguration.java
+++ src/main/java/li/naska/bgg/configuration/WebClientConfiguration.java
@@ -1,11 +1,10 @@
 package li.naska.bgg.configuration;
 
 import java.time.Duration;
-
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
 
@@ -13,16 +12,19 @@ import reactor.netty.resources.ConnectionProvider;
 public class WebClientConfiguration {
 
   @Bean
-  public WebClient.Builder webClientBuilder() {
-    // override Netty HttpClient default behaviour (no max idle time) because connections get closed
-    // by BGG after some time
-    ConnectionProvider connectionProvider = ConnectionProvider.builder("customConnectionPool")
-        .maxIdleTime(Duration.ofMillis(60000))
-        .maxLifeTime(Duration.ofMinutes(5))
-        .build();
+  public WebClientCustomizer webClientCustomizer() {
+    return webClientBuilder -> {
+      // override Netty HttpClient default behaviour (no max idle time) because connections get
+      // closed
+      // by BGG after some time
+      ConnectionProvider connectionProvider = ConnectionProvider.builder("customConnectionPool")
+          .maxIdleTime(Duration.ofMillis(60000))
+          .maxLifeTime(Duration.ofMinutes(5))
+          .build();
 
-    HttpClient httpClient = HttpClient.create(connectionProvider).followRedirect(true);
+      HttpClient httpClient = HttpClient.create(connectionProvider).followRedirect(true);
 
-    return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient));
+      webClientBuilder.clientConnector(new ReactorClientHttpConnector(httpClient));
+    };
   }
 }
