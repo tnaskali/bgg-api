28c7c111297c83dded0661907fb4b5231686b589
Thomas Naskali <thomas@naska.li>
2025-02-06 20:07:14 +0200

    register missing OpenApi runtime hints

--- src/main/java/li/naska/bgg/configuration/OpenApiConfiguration.java
+++ src/main/java/li/naska/bgg/configuration/OpenApiConfiguration.java
@@ -5,13 +5,21 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import jakarta.validation.constraints.NotNull;
+import java.util.Arrays;
+import li.naska.bgg.util.ReflectionUtils;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "BGG API", version = "v1"))
 @SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic")
+@ImportRuntimeHints(OpenApiConfiguration.OpenApiRuntimeHints.class)
 public class OpenApiConfiguration {
 
   static {
@@ -56,5 +64,25 @@ public class OpenApiConfiguration {
         .group("v5 - JSON API (Website)")
         .pathsToMatch("/api/v5/**")
         .build();
+  }
+
+  static class OpenApiRuntimeHints implements RuntimeHintsRegistrar {
+
+    private static final String[] OPENAPI_REFLECTION_CLASSES = {
+      "io.swagger.v3.core.jackson.mixin.Schema31Mixin$TypeSerializer"
+    };
+
+    @Override
+    public void registerHints(@NotNull RuntimeHints hints, ClassLoader classLoader) {
+      Arrays.stream(OPENAPI_REFLECTION_CLASSES)
+          .map(ReflectionUtils::getClass)
+          .forEach(clazz -> hints
+              .reflection()
+              .registerType(
+                  clazz,
+                  MemberCategory.DECLARED_FIELDS,
+                  MemberCategory.INVOKE_DECLARED_METHODS,
+                  MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
+    }
   }
 }
